258fe46460d8a4fdd563901160d42c1c21c991d9
Ykaro Silva <s.ykaro314@gmail.com>
2023-09-27 11:18:07 -0300

    Protjeto inicial

---
+++ src/main/java/br/com/adssearch/AdsSearchApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.adssearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsSearchApiApplication.class, args);
+	}
+
+}
